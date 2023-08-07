9573cc7f641a2909a1caf328a2f57b349c3c459e
Lakshay goyal <akshaybuster5280@gmail.com>
2023-08-07 13:47:16 +0530

    .

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <ClerkProvider
       publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-    >
+    > 
       <html lang="en">
         <body className={inter.className}>
           <Topbar />
