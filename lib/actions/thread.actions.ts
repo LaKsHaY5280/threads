59489966104065ecfcc67ad98118ad4d54abea79
Lakshay goyal <akshaybuster5280@gmail.com>
2023-08-10 21:04:11 +0530

    to comment

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -105,7 +105,46 @@ export async function fetchThreadById(id: string) {
 
     return thread;
   } catch (error) {
-    console.error("Error while fetching thread:", error);
-    throw new Error("Unable to fetch thread");
+    throw new Error(`Error while fetching thread: ${error}`);
+  }
+}
+
+export async function addCommentToThread(
+  threadId: string,
+  commentText: string,
+  userId: string,
+  path: string
+) {
+  connectToDB();
+
+  try {
+    // Find the original thread by its ID
+    const originalThread = await Thread.findById(threadId);
+
+    if (!originalThread) {
+      throw new Error("Thread not found");
+    }
+
+    // Create the new comment thread
+    const commentThread = new Thread({
+      text: commentText,
+      author: userId,
+      parentId: threadId, // Set the parentId to the original thread's ID
+    });
+
+    console.log(commentThread);
+
+    // Save the comment thread to the database
+    const savedCommentThread = await commentThread.save();
+
+    // Add the comment thread's ID to the original thread's children array
+    originalThread.children.push(savedCommentThread._id);
+
+    // Save the updated original thread to the database
+    await originalThread.save();
+
+    revalidatePath(path);
+  } catch (error: any) {
+    throw new Error(`Error while adding comment: ${error}`);
   }
 }
