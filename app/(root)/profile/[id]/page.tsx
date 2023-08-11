3b1818a282fb0420fab8d7cfdaebb10e597e26f0
Lakshay goyal <akshaybuster5280@gmail.com>
2023-08-11 11:42:03 +0530

    profile

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -4,8 +4,7 @@ import { redirect } from "next/navigation";
 import { profileTabs } from "@/constants";
 import ThreadsTab from "@/components/shared/ThreadsTab";
 import ProfileHeader from "@/components/shared/ProfileHeader";
-// import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { fetchUser } from "@/lib/actions/user.actions";
 
 const Page = async ({ params }: { params: { id: string } }) => {
@@ -14,6 +13,7 @@ const Page = async ({ params }: { params: { id: string } }) => {
 
   const userInfo = await fetchUser(params.id);
   if (!userInfo?.onboarded) redirect("/onboarding");
+  console.log(userInfo.threads);
 
   return (
     <section>
@@ -25,6 +25,42 @@ const Page = async ({ params }: { params: { id: string } }) => {
         imgUrl={userInfo.image}
         bio={userInfo.bio}
       />
+      <div className=" mt-9">
+        <Tabs defaultValue="threads" className=" w-full">
+          <TabsList className=" tab">
+            {profileTabs.map((tab) => (
+              <TabsTrigger key={tab.label} value={tab.value} className="tab">
+                <Image
+                  src={tab.icon}
+                  alt={tab.label}
+                  width={24}
+                  height={24}
+                  className=" object-contain"
+                />
+                <p className=" max-sm:hidden">{tab.label}</p>
+                {tab.label === "Threads" && (
+                  <p className=" ml-1 rounded-sm bg-light-4 px-2 py-1 !text-tiny-medium text-light-2">
+                    {userInfo?.threads?.length}
+                  </p>
+                )}
+              </TabsTrigger>
+            ))}
+          </TabsList>
+          {profileTabs.map((tab) => (
+            <TabsContent
+              key={`content-${tab.label}`}
+              value={tab.value}
+              className=" w-full text-light-1"
+            >
+              <ThreadsTab
+                currentUserId={user.id}
+                accountId={userInfo.id}
+                accountType="User"
+              />
+            </TabsContent>
+          ))}
+        </Tabs>
+      </div>
     </section>
   );
 };
