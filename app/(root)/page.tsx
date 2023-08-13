5f55536873e4e552e647b2bc68ab1a6ba7ac6f07
Lakshay goyal <akshaybuster5280@gmail.com>
2023-08-13 12:27:28 +0530

    fixed a home screen bug

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,22 +1,34 @@
-//app/page.tsx
-// import { UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 import ThreadCard from "@/components/cards/ThreadCard";
-import { fetchPosts } from "@/lib/actions/thread.actions";
-import { currentUser } from "@clerk/nextjs";
+// import Pagination from "@/components/shared/Pagination";
 
-export default async function Home() {
-  const result = await fetchPosts(1, 30);
-  const user = await currentUser(); 
-  if (!user) return null;
+import { fetchPosts } from "@/lib/actions/thread.actions";
+import { fetchUser } from "@/lib/actions/user.actions";
+
+async function Home({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
+  const user = await currentUser();
+  if (!user) redirect("/sign-in");
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  const result = await fetchPosts(
+    searchParams.page ? +searchParams.page : 1,
+    30
+  );
 
   return (
-    <div>
-      {/* <UserButton afterSignOutUrl="/"/> */}
-      <h1 className=" head-text text-left">Home</h1>
-      <section className=" mt-9 flex flex-col gap-10">
+    <>
+      <h1 className="head-text text-left">Home</h1>
+
+      <section className="mt-9 flex flex-col gap-10">
         {result.posts.length === 0 ? (
-          <p className=" no-result">No thread found</p>
+          <p className="no-result">No threads found</p>
         ) : (
           <>
             {result.posts.map((post) => (
@@ -29,12 +41,20 @@ export default async function Home() {
                 author={post.author}
                 community={post.community}
                 createdAt={post.createdAt}
-                comments={post.children} 
+                comments={post.children}
               />
             ))}
           </>
         )}
       </section>
-    </div>
+
+      {/* <Pagination
+        path='/'
+        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        isNext={result.isNext}
+      /> */}
+    </>
   );
 }
+
+export default Home;
